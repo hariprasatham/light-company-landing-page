ab5d02db572d83bd07acdeebcc518bfc21538ad1
hariprasatham <hariprasatham@gmail.com>
2023-02-13 13:18:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("btn")
+const light = document.getElementById("light")
+
+function toggleButton(){
+    button.classList.toggle("active")
+    light.classList.toggle("on")
+}
